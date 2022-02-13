b8190f84eee60d72695b383122f6609a3498fe96
Kamigiri <MertDemirbile@outlook.de>
2022-02-13 17:50:16 +0100

    upadtes for dev depoyment

--- src/main/java/com/keraisoft/fd/FdApplication.java
+++ src/main/java/com/keraisoft/fd/FdApplication.java
@@ -1,17 +1,22 @@
 package com.keraisoft.fd;
 
+import org.apache.catalina.connector.Connector;
+import org.apache.coyote.ajp.AbstractAjpProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-public class FdApplication {
+public class FdApplication extends SpringBootServletInitializer {
 
 	private static final Logger log = LoggerFactory.getLogger(FdApplication.class);
 
@@ -19,7 +24,10 @@ public class FdApplication {
 		SpringApplication.run(FdApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(FdApplication.class);
+	}
 
 	@Bean
 	public CommandLineRunner run() throws Exception {
--- src/main/java/com/keraisoft/fd/LoadDatabase.java
+++ src/main/java/com/keraisoft/fd/LoadDatabase.java
@@ -1,6 +1,5 @@
 package com.keraisoft.fd;
 
-import com.keraisoft.fd.csv.FdCsvReader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -21,21 +20,4 @@ class LoadDatabase {
 
     private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
 
-    @Bean
-    CommandLineRunner initDatabase(IssueRepository repository) throws IOException {
-        FdCsvReader reader = new FdCsvReader();
-        List<List<String>> results = reader.getRecords();
-        SimpleDateFormat formatter = new SimpleDateFormat("MMMM dd yyyy", Locale.US);
-
-        /*return args -> {
-            for (int i= 0; i < results.size(); i++) {
-                if( i == 0) {
-                    continue;
-                }
-                //repository.save(new Issue(results.get(i).get(0),results.get(i).get(1), BigDecimal.valueOf(Double.parseDouble(results.get(i).get(2))), results.get(i).get(3), formatter.parse(results.get(i).get(4) + results.get(i).get(5))));
-            }
-            repository.findAll().forEach(issue -> log.info("Preloaded " + issue));
-        };*/
-
-        return null;
-    }}
+}
